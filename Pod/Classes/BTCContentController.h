e83c6725ca8e6c7db27ca41f9b8a3a40b1cede69
gardenofwine <gardenofwine@gmail.com>
2014-07-24 08:51:08 +0300

    BTCContentController init

---
+++ Pod/Classes/BTCContentController.h
@@ -0,0 +1,13 @@
+//
+//  BTCContentController.h
+//  Pods
+//
+//  Created by Benny Weingarten-Gabbay on 7/24/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BTCContentController : NSObject
+
+@end
---
+++ Pod/Classes/BTCContentController.m
@@ -0,0 +1,17 @@
+//
+//  BTCContentController.m
+//  Pods
+//
+//  Created by Benny Weingarten-Gabbay on 7/24/14.
+//
+//
+
+#import "BTCContentController.h"
+
+@implementation BTCContentController
+
++ (void)load {
+    NSLog(@"**== BetterContent!");
+}
+
+@end
