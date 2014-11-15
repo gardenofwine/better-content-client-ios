312b7918ac3be5c0fc9227a975825c5fc2acdc76
gardenofwine <gardenofwine@gmail.com>
2014-11-15 19:18:07 +0200

    UIImage - faster image serializing (by scaling down)

--- Pod/Classes/Scanner/Components/UIImageView+BTCComponent.m
+++ Pod/Classes/Scanner/Components/UIImageView+BTCComponent.m
@@ -7,7 +7,10 @@
 //
 
 #import "UIImageView+BTCComponent.h"
+#import "UIImage+Resize.h"
 
+#define MAX_IMAGE_SIZE 1000
+#define MAX_IMAGE_SIDE_SIZE 50
 @implementation UIImageView (BTCComponent)
 
 - (void)btcIsSerializable{}
@@ -24,11 +27,27 @@
 }
 
 - (NSString *)base64Image{
+    UIImage *theImage = self.image;
     if (self.image != nil) {
-        NSString *imageString = [UIImagePNGRepresentation(self.image) base64EncodedStringWithOptions:0];
+        if ([self shouldResize:self.image.size]){
+            theImage = [self.image resizedImage:[self scaledDownImageSize:self.image.size] interpolationQuality:kCGInterpolationLow];
+        }
+        NSString *imageString = [UIImagePNGRepresentation(theImage) base64EncodedStringWithOptions:0];
         if (imageString != nil) return imageString;
     }
     return @"";
-    
 }
+
+- (BOOL)shouldResize:(CGSize)imageSize{
+    return imageSize.height * imageSize.width >= MAX_IMAGE_SIZE;
+}
+
+- (CGSize)scaledDownImageSize:(CGSize)originalSize{
+    CGFloat maxSide = MAX(originalSize.width, originalSize.height);
+    CGFloat scaleFactor = MAX_IMAGE_SIDE_SIZE / maxSide;
+    
+    return CGSizeMake(originalSize.width * scaleFactor, originalSize.height * scaleFactor);
+}
+                               
+             
 @end
